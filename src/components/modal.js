0cbc12855a1f6ffb57285a0771dcb206917d1ab7
bob <akekbol@gmail.com>
2024-03-19 16:33:43 +0500

    popup fix

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,33 +6,22 @@ const config = {
 export function openPopup(popup) {
   popup.classList.add(config.animatedPopupClass);
   popup.classList.add(config.openPopupClass);
-
-
   document.addEventListener("keydown", closePopupByESC);
 }
 
-// Функция закрытия попапа
 export function closePopup(popup) {
   popup.classList.remove(config.openPopupClass);
-
-  // Откладываем удаление на время работы анимации
-  setTimeout(() => {
-    popup.classList.remove(config.animatedPopupClass);
-  }, 600);
-
+  popup.classList.remove(config.animatedPopupClass); // Добавляем удаление класса анимации
   document.removeEventListener("keydown", closePopupByESC);
 }
 
-// Функция закрытия попапа кнопкой ESC
 function closePopupByESC(evt) {
   if (evt.key === "Escape") {
     const openedPopup = document.querySelector("." + config.openPopupClass);
-
     closePopup(openedPopup);
   }
 }
 
-// Функция закрытия попапа по оверлэю
 export function closePopupByOverlay(evt) {
   if (evt.target.classList.contains(config.openPopupClass)) {
     closePopup(evt.target);
